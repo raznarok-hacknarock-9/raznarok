cbc3b21e765fa523043d076d34bc53775e735899
Michał Kępka <michal.kepka16@gmail.com>
2025-04-12 19:14:39 +0200

    Delete item routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import itemRoutes from './routes/itemRoutes';
 import userRoutes from './routes/userRoutes';
 import chatRoutes from './routes/chatRoutes';
 import { errorHandler } from './middlewares/errorHandler';
@@ -11,7 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/assets', express.static('static'));
 
-app.use('/api/items', itemRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chats', chatRoutes);
 
